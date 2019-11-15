ba16594921330a5cde7129cb19552b7fd96fd70d
David Guevara <37723586+DatWaft@users.noreply.github.com>
2019-11-15 17:24:53 -0600

    comentarios

--- src/semanticanalyzer.py
+++ src/semanticanalyzer.py
@@ -1,4 +1,10 @@
-import sys
+# semanticanalyzer.py
+# Integrantes: David Guevara y Mario Arguello
+# Descripción: contiene un analizador semántico que agarra como input un string
+#              que contiene código de c++ básico y analiza sus errores de scope
+#              y de valor de retorno.
+
+
 import re
 from symboltable import SymbolTable
 
@@ -6,8 +12,33 @@ class SemanticAnalyzer:
   """
   Esta clase es un analizador semántico que se encarga de analizar el string de
   entrada para dar los errores que el mismo contiene.
+
+  Atributos:
+  - file:       contiene el archivo a analizar como un solo string.
+
+  - table:      contiene la tabla de símbolos para poder analizar los símbolos
+                declarados.
+
+  - scopestack: es una lista que funciona como pila para almacenar las funciones
+                dueñas de los scopes anteriores.
+
+  - scope:      contiene la función dueña del scope actual, si es None el scope
+                es global.
+
+  - types:      contiene cada uno de los tipos validos.
+
+  - reserved:   contiene cada una de las palabras reservadas.
+
+  - err:        diccionario que contiene todos los errores con el proposito de
+                usar .format() con ellos.
+
+  - expr:       diccionario que contiene cada una de las expresiones regulares
+                compiladas para facilidad de uso. 
   """
   def __init__(self, string = None):
+    """
+    Inicializa cada una de las variables y agarra un string de archivo.
+    """
     self.file = string
     self.table = SymbolTable()
     self.scopestack = []
@@ -20,6 +51,9 @@ class SemanticAnalyzer:
     self.generate_expressions()
 
   def generate_errors(self):
+    """
+    Cada uno de los erroes preparados para ser usados.
+    """
     self.err['ndcl'] = "\nError - Línea {0}: '{1}' no está declarado."
     self.err['dcl'] = "\nError - Línea {0}: '{1}' ya está declarado."
     self.err['ret'] = ("\nError - Línea {0}: valor de retorno no coincide con la"
@@ -27,35 +61,64 @@ class SemanticAnalyzer:
     self.err['gret'] = "\nError - Línea {0}: retorno a nivel global."
 
   def generate_expressions(self):
+    """
+    Cada una de las expresiones precompiladas preparadas para ser usadas.
+    """
+    # Hace match con expresiones que sea palabras que no estén entre '"'.
     self.expr['var'] = re.compile(r'[A-Za-z]+(?=(?:[^"]*"[^"]*")*[^"]*\Z)')
+    # Hace match von una palabra al inicio.
     self.expr['beg'] = re.compile(r'^[A-Za-z]+')
+    # Hace match con dos palabras al inicio, la segunda palabra se guarda en el
+    # grupo 1.
     self.expr['2nd'] = re.compile(r'^[A-Za-z]+ ([A-Za-z]+)')
+    # Hace match con n carácteres que puede estar separados por un espacio con
+    # unos paréntesis.
+    # El grupo 1 son esos n carácteres y el grupo 2 es lo que está dentro de los
+    # paréntesis.
     self.expr['()'] = re.compile(r'(.+) ?\((.+)\)')
+    # Hace match con n carácteres de new line.
     self.expr['nl'] = re.compile(r'\n+')
 
   def loadfile(self, filename):
+    """
+    Abre el archivo e intenta abrirlo, el atributo file pasa a ser el string del
+    archivo.
+    """
     with open(filename, 'r') as file:
       self.file = file.read()
 
   def setscope(self, scope):
+    """
+    Cambia el dueño del scope actual guardando el anterior en la pila.
+    """
     self.scopestack.append(self.scope)
     self.scope = scope
 
   def retscope(self):
+    """
+    Se devuelve al dueño anterior que había sido guardado en la pila.
+    """
     if len(self.scopestack) == 0:
       self.scope = None
     else:
       self.scope = self.scopestack.pop()
 
   def parsestatement(self, string, line):
+    """
+    Parsea una línea.
+    e.g. int x = 0;
+         string s = "hola"
+         int x, int y, int z
+    """
     if string is None:
       return ""
-
+  
+    # Extrae en un vector todas las variables y tipos.
     v = self.expr['var'].findall(string)
     
-    result = ""
-    prev_type = None
-    for e in v:
+    result = "" # Guarda los errores que se van a devolver.
+    prev_type = None # Se utiliza para ver si el elemento anterior era un tipo.
+    for e in v: 
       if prev_type is not None:
         if e in self.table:
           if prev_type == 'return':
@@ -79,7 +142,12 @@ class SemanticAnalyzer:
     return result
 
   def parsefunction(self, string, line):
+    """
+    Parsea una función.
+    e.g. void funcion(int x, int y, int z)
+    """
     result = ""
+    # Separa las dos partes: 'void funcion' & '(int x, int y, int z)'.
     s1, s2 = self.expr['()'].match(string).groups()
     st = self.expr['beg'].match(string).group(0)
     if st in self.types:
@@ -93,13 +161,17 @@ class SemanticAnalyzer:
       result += self.parsestatement(s1, line)
     else:
       self.setscope(self.scope)
-
+    
+    # Crea el nuevo scope antes para guardar los parámetros ahí.
     self.table = self.table.newscope()
 
     result += self.parsestatement(s2, line)
     return result
 
   def parse(self):
+    """
+    Parsea un archivo entero hasta el final y devuelve los errores encontrados.
+    """
     result = ""
     source = self.file
     source = re.sub(r'\t+', '', source) # Esto quita los carácteres \t'.
@@ -108,15 +180,15 @@ class SemanticAnalyzer:
     line = 1
 
     while(len(source) > 0):
-      # Paso 1. Verificar si es más largo el string hasta el siguiente ';' o
-      # hasta el siguiente '{'.
-
+      # Esto se usa para ver cuáles elementos existen y cuál de los existentes
+      # es el más cercano.
       len1 = -1
       len2 = -1
       len3 = -1
       
       v = []
 
+      # Si existe lo añade al vector de elementos existentes.
       if ';' in source:
         len1 = source.index(';')
         v += [len1]
@@ -129,46 +201,28 @@ class SemanticAnalyzer:
       
       this = min(v)
     
+      #  Dice el número de línea según cuántos '\n' se han econtrado.
       if '\n' in source[:this]:
         line += source[:this].count('\n')
 
-      if this == len1:
+      if this == len1: # Si el más cercano fue un ';'.
         result += self.parsestatement(self.expr['nl'].sub('',
           source[:this].strip()), line)
-      elif this == len2:
+      elif this == len2: # Si el más cercano fue un '{'.
         result += self.parsefunction(self.expr['nl'].sub('',
           source[:this].strip()), line)
-      else:
+      else: # Si el más cercano fue un '}'.
         self.retscope()
         self.table = self.table.getfather()
         
+      # Elimina lo que se acaba de parsear junto con el carácter delimitador.
       source = source[this + 1:]
 
+    # Reinicia el scope.
     self.table = SymbolTable()
     self.scope = None
     # Para eliminar las línea repetidas.
     l = list(dict.fromkeys(re.sub(r'^\n', '', result).split('\n')))
     res = ''.join(s + '\n' for s in l)
     return re.sub(r'\n$', '', res)
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    file_name = sys.argv[1]
-  else:
-    file_name = '../data/test2.txt'
-    print("Se puede introducir el nombre del archivo como parámetro.")
-  print("Archivo seleccionado: ", file_name)
-
-  sa = SemanticAnalyzer()
-  try:
-    sa.loadfile(file_name)
-    v = sa.file.split('\n')
-    print('──┐')
-    for i,line in enumerate(v):
-      print(('0' if i+1 < 10 else '') + str(i+1) + "│" + line)
-    print('──┘')
-    result = sa.parse()
-    print("No ha habido ningún error." if result == "" else result)
-  except:
-    print("Nombre de archivo invalido")
 
--- src/symboltable.py
+++ src/symboltable.py
@@ -1,3 +1,7 @@
+# symboltable.py
+# Integrantes: David Guevara y Mario Arguello
+# Descripción: contiene una implementación de una tabla de símbolos.
+
 class Entry:
   """
   Una entrada de la tabla de símbolos.
@@ -28,21 +32,40 @@ class Entry:
 class SymbolTable:
   """
   Una tabla de símbolos.
+  
+  Sirve para almacenar entradas de la tabla de símbolos.
+
+  Tiene un padre para que se pueda derivar una tabla de símbolos de otra y que
+  la nueva igual tenga acceso a las entradas de la anterior, esto permite crear
+  scopes.
+
+  Atributos:
+  - _table: un diccionario usado para almacenar cada entrada de la tabla actual.
+  - _father: la tabla de símbolos anterior a la actual.
   """
   def __init__(self):
     """
     Inicializa la tabla de simbolos.
+    Si la tabla fue creada por este método, no va a tener padre.
     """
     self._table = dict()
     self._father = None
   
   def symbols(self):
+    """
+    Devuelve una lista de entadas de la tabla, tanto las de la tabla actual como
+    las de las anteriores a esta.
+    Primero van las entradas del padre y luego las nuevas.
+    """
     l = list(self._table.values())
     if self._father is not None:
       l = self._father.symbols() + l
     return l
 
   def __iter__(self):
+    """
+    Permite hacer list(SymbolTable).
+    """
     for i in self.symbols():
       yield i
 
@@ -56,6 +79,8 @@ class SymbolTable:
   def delete(self, symbol):
     """
     Si el elemento está presente, lo elimina de la tabla.
+    El elemento puede estar presente en la tabla actual o en el padre que igual
+    lo elimina.
     """
     if symbol in self:
       if symbol in self._table:
@@ -66,21 +91,29 @@ class SymbolTable:
   def lookup(self, symbol):
     """
     Devuelve un elemento de la tabla.
+    Se fija primero si el elemento está, y una vez sabe que está se fija en el
+    padre de ser posible, y luego en la actual.
     """
     if symbol in self:
       if self._father is not None:
         aux = self._father.lookup(symbol)
         if aux is None:
           return self._table[symbol]
+        else:
+          return aux
       return self._table[symbol]
     return None
 
   def __getitem__(self, key):
+    """
+    Sobrecarga para poder hacer SymbolTable['EntryName'].
+    """
     return self.lookup(key)
   
   def __contains__(self, key):
     """
-    Dice si un elemento está presente en la tabla.
+    Dice si un elemento está presente en la tabla, puede estar en el padre o en
+    la actual.
     """
     if self._father is not None:
       return key in self._table or key in self._father
@@ -89,6 +122,7 @@ class SymbolTable:
   def newscope(self):
     """
     Crea un nuevo 'scope'.
+    Devuelve una nueva tabla de símbolos con la actual como anterior.
     """
     aux = SymbolTable()
     aux._father = self
